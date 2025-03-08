fe30e12a8a5bc3d4f57224372b4560ef7e0a8bc7
Amr Mubarak <138404703+amrrdev@users.noreply.github.com>
2025-03-09 00:04:21 +0200

    [88] [Time Beats: 100.00%] [Memory Beats: 83.32%] - LeetPush

--- Merge-Sorted-Array.go
+++ Merge-Sorted-Array.go
@@ -1,11 +1,27 @@
 
 func merge(nums1 []int, m int, nums2 []int, n int) {
-\ti := m
-\tfor _, value := range nums2 {
-\t\tnums1[i] = value
+\tfirst, second := 0, 0
+\tnum1Values := append([]int{}, nums1[:m]...)
+
+\ti := 0
+\tfor first < m && second < n {
+\t\tif num1Values[first] < nums2[second] {
+\t\t\tnums1[i] = num1Values[first]
+\t\t\tfirst++
+\t\t} else {
+\t\t\tnums1[i] = nums2[second]
+\t\t\tsecond++
+\t\t}
 \t\ti++
 \t}
-
-\tslices.Sort(nums1)
-
+\tfor first < m {
+\t\tnums1[i] = num1Values[first]
+\t\tfirst++
+\t\ti++
+\t}
+\tfor second < n {
+\t\tnums1[i] = nums2[second]
+\t\tsecond++
+\t\ti++
+\t}
 }
