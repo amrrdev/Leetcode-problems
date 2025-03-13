13e2f726d5c5db022fe17ffb0449cb929e194db8
Amr Mubarak <138404703+amrrdev@users.noreply.github.com>
2025-03-13 20:58:42 +0200

    [121] [Time Beats: 100.00%] [Memory Beats: 59.05%] - LeetPush

--- Best-Time-to-Buy-and-Sell-Stock.go
+++ Best-Time-to-Buy-and-Sell-Stock.go
@@ -1,10 +1,11 @@
 func maxProfit(prices []int) int {
-\tbuy, seal := prices[0], 0
-
+\tbuy := prices[0]
+\tseal := 0
 \tfor i := 1; i < len(prices); i++ {
 \t\tif prices[i] < buy {
 \t\t\tbuy = prices[i]
-\t\t} else {
+\t\t}
+\t\tif prices[i] > buy {
 \t\t\tseal = max(seal, prices[i]-buy)
 \t\t}
 \t}
