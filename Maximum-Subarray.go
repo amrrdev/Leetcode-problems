4cbbbe7c3e1e9de1e5ff5435af4f2885fd9cbc79
Amr Mubarak <138404703+amrrdev@users.noreply.github.com>
2025-10-12 10:05:23 +0300

    [53] [Time Beats: 100.00%] [Memory Beats: 62.08%] - LeetPush

--- Maximum-Subarray.go
+++ Maximum-Subarray.go
@@ -1,13 +1,11 @@
+func maxSubArray(arr []int) int {
+	currSum := arr[0]
+	maxSum := arr[0]
 
-func maxSubArray(nums []int) int {
-\tcurSum, maxSum := nums[0], nums[0]
-\tfor i := 1; i < len(nums); i++ {
-\t\tif curSum+nums[i] > nums[i] {
-\t\t\tcurSum += nums[i]
-\t\t} else {
-\t\t\tcurSum = nums[i]
-\t\t}
-\t\tmaxSum = max(maxSum, curSum)
-\t}
-\treturn maxSum
+	for i := 1; i < len(arr); i++ {
+		currSum = max(arr[i], currSum+arr[i])
+		maxSum = max(currSum, maxSum)
+	}
+
+	return maxSum
 }
