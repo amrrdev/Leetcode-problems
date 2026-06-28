268d692871ff024c5898498e0a5fdc8c75ad8283
Amr Mubarak <138404703+amrrdev@users.noreply.github.com>
2026-06-28 23:57:48 +0300

    [1929] [Time Beats: 100.00%] [Memory Beats: 28.51%] - LeetPush

--- Concatenation-of-Array.go
+++ Concatenation-of-Array.go
@@ -1,12 +1,6 @@
-1func getConcatenation(nums []int) []int {
-2	res := make([]int, len(nums)*2)
-3	n := len(nums)
-4
-5	for i := range nums {
-6		res[i] = nums[i]
-7		res[i+n] = nums[i]
-8	}
-9
-10	return res
-11}
-12
+1import "fmt"
+2
+3func getConcatenation(nums []int) []int {
+4    nums = append(nums, nums...)
+5    return nums
+6}
