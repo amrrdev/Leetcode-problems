de655a394868f123f15d134b81b1ce846dca0322
Amr Mubarak <138404703+amrrdev@users.noreply.github.com>
2026-06-29 20:56:58 +0300

    [242] [Time Beats: 100.00%] [Memory Beats: 28.51%] - LeetPush

--- Valid-Anagram.go
+++ Valid-Anagram.go
@@ -1,19 +1,18 @@
 1func isAnagram(s string, t string) bool {
-2	if len(s) != len(t) {
-3		return false
-4	}
-5
-6	cnt := [26]int{}
-7	for i := range s {
-8		cnt[int(s[i]-'a')]++
-9		cnt[int(t[i])-'a']--
-10	}
-11
-12	for i := range 26 {
-13		if cnt[i] != 0 {
-14			return false
-15		}
-16	}
-17
-18	return true
-19}
+2    m := make([]int, 200)
+3    for _, v := range s {
+4        intValue := int(v)
+5        m[intValue] += 1
+6    }
+7    for _, v := range t {
+8        intValue := int(v)
+9        m[intValue] -= 1
+10    }
+11    for _, v := range m {
+12        if v != 0 {
+13            return false
+14        }
+15    }
+16    return true
+17}
+18
